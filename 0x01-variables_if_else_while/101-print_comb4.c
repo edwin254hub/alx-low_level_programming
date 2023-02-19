b05e3cd99e06771c197f4b5e6bf89fdbbccec6f6
edwin254hub <edwinmutunga42@gmail.com>
2023-02-19 04:01:41 -0800

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of three digits
+ * the three digits must be different
+ * print only the smallest combination of three digits
+ * numbers should be printed in ascending order, with three digits
+ * you can only use putchar six times maximum in your code
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int c;
+	int d;
+	int e = 0;
+
+	while (e < 10)
+	{
+		d = 0;
+		while (d < 10)
+		{
+			c = 0;
+			while (c < 10)
+			{
+				if (c != d && d != e && e < d && d < c)
+				{
+					putchar('0' + e);
+					putchar('0' + d);
+					putchar('0' + c);
+
+					if (c + d + e != 9 + 8 + 7)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				c++;
+			}
+			d++;
+		}
+		e++;
+	}
+	putchar('\n');
+	return (0);
+}
