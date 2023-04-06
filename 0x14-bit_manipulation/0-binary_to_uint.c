f9f30934a693ff89fc74578f67e1985ab7eaa521
edwin254hub <edwinmutunga42@gmail.com>
2023-04-06 05:11:05 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,6 +14,9 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int decimal = 0;
 	int len = strlen(b);
 
+	if (!b)
+		return (0);
+
 	for (i = 0; i < len; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
